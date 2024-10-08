9dd178952c4d58b716fccedb18e34d197ace3146
pablo-andersen <pandersen@alumnos.exa.unicen.edu.ar>
2024-10-07 23:12:48 -0300

    configuro application.properties y agrego un controller basico qeu funciona con el endpoint /hola

---
+++ src/main/java/org/example/grupo20integrador3/controller/HolaMundoController.java
@@ -0,0 +1,12 @@
+package org.example.grupo20integrador3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+    @GetMapping("/hola")
+    public String holaMundo() {
+        return "Hola Mundo";
+    }
+}
